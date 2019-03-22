63f77315d12082ecbeedb45e20d321f09cd30565
Sheva Xu <shevaxu@outlook.com>
2019-03-22 22:35:33 +0800

    refactor (pay): use infra

--- cmd/paymentservice/go.mod
+++ cmd/paymentservice/go.mod
@@ -1,10 +1,8 @@
-module github.com/practigo/hipstershop/paymentservice
+module github.com/practigo/hs-paymentservice
 
 require (
 	github.com/google/uuid v1.1.1
-	github.com/practigo/hipstershop v0.1.1
+	github.com/practigo/hipstershop v0.2.1
 	github.com/sirupsen/logrus v1.3.0
-	go.opencensus.io v0.19.0
-	golang.org/x/net v0.0.0-20181217023233-e147a9138326
-	google.golang.org/grpc v1.19.0
+	golang.org/x/net v0.0.0-20190125091013-d26f9f9a57f3
 )
--- cmd/paymentservice/main.go
+++ cmd/paymentservice/main.go
@@ -3,76 +3,49 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
-	"time"
 
 	"github.com/sirupsen/logrus"
-	"go.opencensus.io/plugin/ocgrpc"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/health"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/reflection"
 
 	pb "github.com/practigo/hipstershop/genproto"
-	cc "github.com/practigo/hipstershop/paymentservice/creditcard"
+	"github.com/practigo/hipstershop/infra"
+	cc "github.com/practigo/hs-paymentservice/creditcard"
 )
 
 const (
-	defaultPort = "50051"
+	defaultPort = 50051
 )
 
-var log *logrus.Logger
-
-func init() {
-	log = logrus.New()
-	log.Level = logrus.DebugLevel
-	log.Formatter = &logrus.JSONFormatter{
-		FieldMap: logrus.FieldMap{
-			logrus.FieldKeyTime:  "timestamp",
-			logrus.FieldKeyLevel: "severity",
-			logrus.FieldKeyMsg:   "message",
-		},
-		TimestampFormat: time.RFC3339Nano,
-	}
-	log.Out = os.Stdout
-}
-
-var charger cc.Charger
-
 func main() {
-	port := defaultPort
-	if value, ok := os.LookupEnv("APP_PORT"); ok {
-		port = value
-	}
-	port = fmt.Sprintf(":%s", port)
+	log := infra.InitLogrus()
 
-	lis, err := net.Listen("tcp", port)
+	addr := fmt.Sprintf(":%d", infra.AppPort(defaultPort))
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	srv := grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
-	svc := &server{}
-	pb.RegisterPaymentServiceServer(srv, svc)
-	healthpb.RegisterHealthServer(srv, health.NewServer())
-	log.Infof("Payment Service listening on port %s", port)
 
-	// Register reflection service on gRPC server.
-	reflection.Register(srv)
+	srv := infra.NewServer(infra.WithHealth, infra.WithReflection)
+	pb.RegisterPaymentServiceServer(srv, &server{
+		ch: &cc.NoOps{},
+		lg: log,
+	})
 
-	charger = &cc.NoOps{} // TODO: so far it is just a mock
-
+	log.Infof("Payment Service listening on %s", addr)
 	if err := srv.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
 // server controls RPC service responses.
-type server struct{}
+type server struct {
+	ch cc.Charger
+	lg *logrus.Logger
+}
 
 func (s *server) Charge(ctx context.Context, req *pb.ChargeRequest) (*pb.ChargeResponse, error) {
-	log.Info("[Charge] received request")
-	defer log.Info("[Charge] completed request")
+	s.lg.Info("[Charge] received request")
+	defer s.lg.Info("[Charge] completed request")
 	// convert
 	card := cc.Card{
 		Number: req.CreditCard.CreditCardNumber,
@@ -82,7 +55,7 @@ func (s *server) Charge(ctx context.Context, req *pb.ChargeRequest) (*pb.ChargeR
 			Month: int(req.CreditCard.CreditCardExpirationMonth),
 		},
 	}
-	id, _ := charger.Charge(card, req.Amount)
+	id, _ := s.ch.Charge(card, req.Amount)
 	return &pb.ChargeResponse{
 		TransactionId: id,
 	}, nil
